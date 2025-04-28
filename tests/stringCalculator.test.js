f0658af9bb419bc19673d84c16380d9c827e7695
Manoj kumar <53403891+manoj-kumar-vst-au4@users.noreply.github.com>
2025-04-28 12:07:03 +0530

    single number and comma seperated number test case passed

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -11,6 +11,17 @@ describe('StringCalculator', () => {
     expect(calculator.add('')).toBe(0);
   });
 
+  test('should return the number itself when only one number is provided', () => {
+    expect(calculator.add('1')).toBe(1);
+    expect(calculator.add('5')).toBe(5);
+  });
+  test('should return sum of two comma-separated numbers', () => {
+    expect(calculator.add('1,2')).toBe(3);
+    expect(calculator.add('10,20')).toBe(30);
+  });
 
+  test('should return sum of multiple comma-separated numbers', () => {
+    expect(calculator.add('1,2,3,4,5')).toBe(15);
+  });
   
 });
